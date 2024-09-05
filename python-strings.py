abe177798c9092c9dcc1f38519d1004421ff19f8
johnmaicon92 <johnmaicon_sempre@hotmail.com>
2024-09-05 19:44:27 -0400

    final string

--- python-strings.py
+++ python-strings.py
@@ -40,8 +40,6 @@ for result in results:
     print()
 
 
-reviews = [ "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "Poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it."]
-
 positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"]
 negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
 ordinary_words = ["average", "standard", "median", "normal", "typical", "regular"]
